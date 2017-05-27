3db1d7392336fe6ca92ffe8068fedd44fa696109
lveslxl@gmail.com <lveslxl@gmail.com>
2017-05-27 11:15:17 +0800

    更改目录结构

--- AddressSelection/AddressView/Model/Province.swift
+++ AddressSelection/AddressView/Model/Province.swift
@@ -9,12 +9,12 @@
 import UIKit
 import MJExtension
 class City: NSObject {
-    var city:String?
-    var areas:[String]?
+    var city:String?    //市
+    var areas:[String]? //区
 }
 
 class Province: NSObject {
-    var state:String? //省
+    var state:String?   //省
     var cities:[City]?
     override static func mj_objectClassInArray() -> [NSObject : AnyObject]! {
         return ["cities": NSStringFromClass(City.self)]
